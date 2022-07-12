ae923b00b3e4de2621ee398967795506bc0e625f
zverchyk <zverchino@gmail.com>
2022-07-12 17:54:56 +0200

    ye

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function loadPic(numPic = 30){
+    var i = 1;
+    while(i<numPic)
+        {
+        var element = document.getElementById("giv");
+        var clone = element.cloneNode(true);
+        clone.id ="giv"+i;
+        element.after(clone);
+        i= i+1;
+        }
+}
+loadPic()
+
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        loadPic()
+    }
+})
